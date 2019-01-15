bf417dd1d4b24ec2f249c601e79d45dd7adbfbad
lauramann <lauramann@trentu.ca>
2019-01-15 15:32:54 -0500

    Added header

---
+++ shopify_challenge/src/Search.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+// import "./Header.css";
+
+class Header extends PureComponent {
+  render() {
+    return (
+        <form>
+        <textarea/>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+export default Header;
